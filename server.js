cae2677e53b6eedad2010fb04ca25d68a516edfc
Sanjay-13 <69335575+Sanjay-13@users.noreply.github.com>
2021-11-04 09:15:23 -0500

    second commit

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.static('./dist/topicangular'));
 
 app.get('/*',(req,res) =>
-    res.sendFile('index.html',{root:'dist/topicangular'}),
+    res.sendFile('index.html',{root:'dist/topicangular/'}),
 );
 
 // Start the app by listening on the default Heroku port
